0182364c6b4942a492e5131e22b22d98e18738f1
Sil-Duckers <s.f.duckers@gmail.com>
2021-12-09 19:42:24 +0100

    lines progression

--- ID-5ExpertiseAreas/Assets/GeneralCodeLV1.cs
+++ ID-5ExpertiseAreas/Assets/GeneralCodeLV1.cs
@@ -29,6 +29,10 @@ public class GeneralCodeLV1 : MonoBehaviour
     public GameObject Amanda2;
     public GameObject Alice2;
 
+    private LineRenderer lineRend; 
+    private Vector2 mousePos;
+    private Vector2 startMousePos;
+
     public TextMeshProUGUI GeneralText;
 
     // Start is called before the first frame update
@@ -43,41 +47,71 @@ public class GeneralCodeLV1 : MonoBehaviour
         GameObject Amanda1 = Instantiate(Amanda, P5);
         GameObject Alice1 = Instantiate(Alice, P6);
 
+        lineRend = GetComponent<LineRenderer>();
+        lineRend.positionCount = 2;
+
         StartCoroutine(StartGame());
 
     }
+
 
     IEnumerator StartGame()
     {
         yield return new WaitForSeconds(3f);
         GeneralText.text = "Please Match the prompts with the persons";
 
-    } 
+    }
+    
+    public void Choice1()
+    {
+        lineRend.SetPosition(0, new Vector3(-14, 6, 10f));
+    }
+
+    public void Choice2()
+    {
+        lineRend.SetPosition(0, new Vector3(-5, 6, 10f));
+    }
+
+    public void Choice3()
+    {
+        lineRend.SetPosition(0, new Vector3(5, 6, 10f));
+    }
+
+    public void Choice4()
+    {
+        lineRend.SetPosition(0, new Vector3(14, 6, 10f));
+    }
 
     public void MarcoClick()
     {
+        lineRend.SetPosition(1, new Vector3(-15, -3, 9f));
         StartCoroutine(MarcoClick2());
+        //startMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
     }
-
     public void LilyClick()
     {
+        lineRend.SetPosition(1, new Vector3(-9, -3, 9f));
         StartCoroutine(LilyClick2());
     }
-    
     public void JeffClick()
     {
+        lineRend.SetPosition(1, new Vector3(-3, -3, 9f));
         StartCoroutine(JeffClick2());
     }
     public void GunterClick()
     {
+        lineRend.SetPosition(1, new Vector3(3, -3, 9f));
         StartCoroutine(GunterClick2());
     }
     public void AmandaClick()
     {
+        lineRend.SetPosition(1, new Vector3(9, -3, 9f));
         StartCoroutine(AmandaClick2());
     }
     public void AliceClick()
     {
+        lineRend.SetPosition(1, new Vector3(15, -3, 9f));
         StartCoroutine(AliceClick2());
     }
 
@@ -86,6 +120,10 @@ public class GeneralCodeLV1 : MonoBehaviour
         Marco2.SetActive(true);
         yield return new WaitForSeconds(3f);
         Marco2.SetActive(false);
+        /*mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lineRend.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+        lineRend.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));*/
+        
     }
 
     IEnumerator LilyClick2()
